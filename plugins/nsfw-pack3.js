ba0b1911b102dfdba35c96762ba92eb203769e43
Wichi <geraldorincon52@gmail.com>
2025-08-22 11:52:53 -0400

    Actualizar nsfw-pack3.js

--- plugins/nsfw-pack3.js
+++ plugins/nsfw-pack3.js
@@ -2,34 +2,29 @@ import axios from "axios";
 import fs from "fs";
 
 let handler = async (m, { conn, usedPrefix, command}) => {
-  let chatId = m.chat;
-
-  let activos = fs.existsSync('./activos.json')
-? JSON.parse(fs.readFileSync('./activos.json', 'utf-8'))
-: {};
-
   try {
     let res = await axios.get("https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/booty.json");
     let data = res.data;
     let url = data[Math.floor(Math.random() * data.length)];
 
-    await conn.sendMessage(m.chat, {
+    let message = {
       image: { url},
       caption: "🔥 Aquí tienes un *pack* 🔞",
       footer: '𝖯𝗋𝖾𝖼𝗂𝗈𝗇𝖺 𝖾𝗅 𝖻𝗈𝗍𝗈́𝗇 𝗉𝖺𝗋𝖺 𝗅𝖺 𝗌𝗂𝗀𝗎𝗂𝖾𝗇𝗍𝖾 𝗂𝗆𝖺𝗀𝖾𝗇',
-      templateButtons: [
+      buttons: [
         {
-          index: 1,
-          quickReplyButton: {
-            displayText: '𝖲𝗂𝗀𝗎𝗂𝖾𝗇𝗍𝖾',
-            id: `${usedPrefix + command}`
+          buttonId: usedPrefix + command,
+          buttonText: { displayText: '𝖲𝗂𝗀𝗎𝗂𝖾𝗇𝗍𝖾'},
+          type: 1
 }
-}
-      ]
-}, { quoted: m});
+      ],
+      headerType: 4
+};
+
+    await conn.sendMessage(m.chat, message, { quoted: m});
 
 } catch (e) {
-    console.error("❌ Error en.pack:", e);
+    console.error("❌ Error en pack5:", e);
     await conn.reply(m.chat, "❌ No se pudo obtener el contenido.", m);
 }
 };
