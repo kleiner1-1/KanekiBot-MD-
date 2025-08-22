5b59b9bf2a3869940a69a5344e3cf8a287bffe7a
Wichi <geraldorincon52@gmail.com>
2025-08-22 11:22:55 -0400

    Actualizar nsfw-pack3.js

---
+++ plugins/nsfw-pack3.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import fs from "fs";
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let chatId = m.chat;
+
+  let activos = fs.existsSync('./activos.json')
+    ? JSON.parse(fs.readFileSync('./activos.json', 'utf-8'))
+    : {};
+
+  try {
+    let res = await axios.get("https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/booty.json");
+    let data = res.data;
+    let url = data[Math.floor(Math.random() * data.length)];
+
+    await conn.sendMessage(m.chat, {
+      image: { url },
+      caption: "🔥 Aquí tienes un *pack* 🔞",
+      footer: '𝖯𝗋𝖾𝖼𝗂𝗈𝗇𝖺 𝖾𝗅 𝖻𝗈𝗍𝗈́𝗇 𝗉𝖺𝗋𝖺 𝗅𝖺 𝗌𝗂𝗀𝗎𝗂𝖾𝗇𝗍𝖾 𝗂𝗆𝖺𝗀𝖾𝗇',
+      buttons: [
+        {
+          buttonId: `${usedPrefix + command}`,
+          buttonText: { displayText: '𝖲𝗂𝗀𝗎𝗂𝖾𝗇𝗍𝖾' },
+          type: 1
+        }
+      ]
+    }, { quoted: m });
+
+  } catch (e) {
+    console.error("❌ Error en .pack:", e);
+    await conn.reply(m.chat, "❌ No se pudo obtener el contenido.", m);
+  }
+};
+
+handler.command = ["pack5"];
+handler.tags = ["nsfw"];
+handler.help = ["pack5"];
+handler.register = false; 
+
+export default handler;
