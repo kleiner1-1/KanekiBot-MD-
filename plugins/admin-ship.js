ef499bfa9eed34ba510f23078e79fdf03b19bbe8
Bajobots1-1 <kleinergalindo4@gmail.com>
2026-03-21 01:27:27 -0500

    Refactor user mention detection in admin-ship.js
    Updated user mention handling and improved error messages.

--- plugins/admin-ship.js
+++ plugins/admin-ship.js
@@ -1,17 +1,19 @@
-let handler = async (m, { conn, mentionedJid }) => {
+let handler = async (m, { conn }) => {
     if (!m.isGroup) throw 'Solo en grupos'
 
     global.db.data.ship = global.db.data.ship || {}
 
     let user1 = m.sender
-    let user2 = mentionedJid[0]
 
-    if (!user2) throw 'Menciona a alguien'
+    // 👇 detectar mención correctamente
+    let user2 = m.mentionedJid?.[0] || 
+                m.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0]
+
+    if (!user2) throw 'Menciona a alguien\nEj: .ship @usuario'
 
     // porcentaje
     let porcentaje = Math.floor(Math.random() * 101)
 
-    // mensaje simple
     let estado = 'No se sabe...'
     if (porcentaje <= 20) estado = 'No combinan mucho'
     else if (porcentaje <= 50) estado = 'Puede funcionar'
@@ -30,7 +32,6 @@ let handler = async (m, { conn, mentionedJid }) => {
 
     global.db.data.ship[chat] = global.db.data.ship[chat].slice(-15)
 
-    // imagen simple
     let img = `https://api.popcat.xyz/ship?user1=${user1.split('@')[0]}&user2=${user2.split('@')[0]}`
 
     let txt = `@${user1.split('@')[0]} ❤️ @${user2.split('@')[0]}\n`
