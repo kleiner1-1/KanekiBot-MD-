9b1dcfe8e6b77d80e57aadf3962ecf183640e837
Bajobots1-1 <kleinergalindo4@gmail.com>
2026-04-23 15:43:12 -0500

    Fix import statement and update export syntax

--- plugins/Crash_homeandr.js
+++ plugins/Crash_homeandr.js
@@ -1,3 +1,5 @@
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) {
     return conn.reply(m.chat, `❌ *Uso incorrecto*
